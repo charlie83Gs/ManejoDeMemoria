3b2e2b9c75a0414ea87405b438b83ce10f50f11b
Carlos <carlosgomezsoza@gmail.com>
2019-10-13 14:54:46 -0600

    first commit

---
+++ src/Main/ManejoDeMemoria.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+/**
+ *
+ * @author Charlie
+ */
+import processing.core.PApplet;
+
+public class ManejoDeMemoria extends PApplet {
+    
+    public static void main(String[] args){
+        PApplet.main("Main.ManejoDeMemoria");
+    }
+    
+    public void settings(){
+        size(300,300);
+    }
+    
+    @Override
+    public void setup() {
+        stroke(155, 0, 0);
+    }
+
+    @Override
+    public void draw() {
+        line(mouseX, mouseY, width / 2, height / 2);
+    }
+}
